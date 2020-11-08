07b4909c7d64f98e86ef540d5abd14899cd5b7c5
Nikita Yakuschenko <nikita.yakuschenko@gmail.com>
2020-11-08 19:24:44 +0300

    master

--- app/app.js
+++ app/app.js
@@ -3,5 +3,10 @@
 const cars = ["Mazda", "Ford", "BMW", "Mercedes"];
 const fib = [0, 1, 1, 2, 3, 5, 8, 13];
 
+// Method
+cars.push("Renault", "Porsche");
+cars.unshift("Volga", "Lada");
+
+// Function
+function addItemToEnd() {}
 console.log(cars);
-console.log(fib.length);
